8bb64774c5e0d077eba5dd47481544662faa7b1e
trouman1 <101309243+trouman1@users.noreply.github.com>
2022-05-05 14:08:21 +0800

    Update IDS_syn.py

--- scan/IDS_syn.py
+++ scan/IDS_syn.py
@@ -1,38 +1,69 @@
 import os
 import time
+from collections import Counter
 
 
-def check_syn():
-    blacklist = []  # 定义一个黑名单
-    ip_suspicion = []  # 定义一个可疑ip名单
-    SYN_RECV_sum = os.popen("netstat -ant | grep SYN_RECV | wc -l").read()  # 查看SYN_RECV报文是否有异常
-    if int(SYN_RECV_sum) > 50:
-        SYN_RECV_ip = os.popen(
-            "netstat -ant | grep SYN_RECV | awk '{print $5}'|awk -F : '{print $1}'").readlines()  # 获取异常报文的IP
-        for i in set(SYN_RECV_ip):
-            ip_suspicion.append(i.split())
-    for ip in ip_suspicion:
-        ip = str(ip)[2:-2]
-        if ip == '127.0.0.1':
-            pass
+def get_cpu_load():  # 获取CPU一分钟内的平均负载，砍掉（反应太慢，影响判断）
+    cpu_load = float(os.popen("uptime | awk -F ': ' '{print $2}' | awk -F ',' '{print $1}'").read())
+    # print('cpu:', cpu_load)
+    return cpu_load
+
+
+def get_conn_count():  # 获取netstat -ant的连接数量
+    conn_count = int(os.popen('netstat -ant | wc -l').read())
+    # print('coon:', conn_count)
+    return conn_count
+
+
+def get_syn_recv():  # 获取recv报文的数量
+    recvq = int(os.popen("ss -lnt | grep :80 | awk '{print $2}'").read())
+    # print('recv:', recvq)
+    return recvq
+
+
+def get_net_bandwidth():  # 获取接收流量，取十次，得平均数
+    # flow = int(os.popen("ifconfig ens33 | grep 'RX packets' | awk '{printf $5}'").read())
+    flow_10 = []
+    subtraction = []
+    for i in range(10):
+        flow_sum = int(os.popen("ifconfig ens33 | grep 'RX packets' | awk '{printf $5}'").read())
+        flow_10.append(flow_sum)
+        time.sleep(0.1)
+    for f in range(len(flow_10)):
+        if f != 9:
+            i = flow_10[f + 1] - flow_10[f]
+            subtraction.append(i)
         else:
-            suspicion_count = os.popen(f"netstat -ant | grep SYN_RECV | grep {ip} |wc -l").read()  # 判断IP是否是攻击行为
-            if int(suspicion_count) > 20:
-                blacklist.append(ip)  # 如果属于攻击行为就加入黑名单
-    return blacklist
-
-
-def do_ids():
-    blacklist = check_syn()
-    for blackip in blacklist:
-        try:
-            os.popen(f"firewall-cmd --add-rich-rule='rule family=ipv4 source address={blackip} drop' --timeout=10m")
-            print(f"检测到{blackip}正在攻击服务器，已拉黑10分钟")
-        except:
             pass
+    average = (subtraction[0] + subtraction[1] + subtraction[2] + subtraction[3] + subtraction[4] + subtraction[5] +
+               subtraction[6] + subtraction[7] + subtraction[8]) / 9
+    # print(int(average))
+    return int(average)
+
+
+def get_most_ip():  # 获取连接数最多的IP
+    ip_list = os.popen("netstat -ant | grep :80 | awk '{print $5}'").read().split('\n')
+    count_ip = []
+    for c_ip in ip_list:
+        count_ip.append(c_ip.split(':')[0])
+    rank = Counter(count_ip)
+    most_ip = rank.most_common()
+    # print(most_ip[0][0])
+    return most_ip[0][0]
+
+
+def join_the_blacklist(i):  # 加入防火墙黑名单
+    os.popen(f"firewall-cmd --add-rich-rule='rule family=ipv4 source address={i} drop' --timeout=10m")
+    print(f"检测到{i}正在攻击服务器，已拉黑10分钟")
 
 
 if __name__ == '__main__':
     while True:
-        do_ids()
-        time.sleep(1)
+        conn = get_conn_count()
+        recv = get_syn_recv()
+        flow = get_net_bandwidth()
+        print(f"TCP Conn: {conn}, REVC_SYN: {recv}, Flow: {flow}")
+        if conn > 500 and recv > 50 and flow > 5000:
+            ip = get_most_ip()
+            join_the_blacklist(ip)
+        time.sleep(5)
